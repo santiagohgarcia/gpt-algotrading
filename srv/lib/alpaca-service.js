69241243189310c837d2d1fc9662a9ddd3fdc711
shgarcia <santiago.garcia@un.org>
2024-12-03 17:41:39 -0500

    technical indicators RSI and SMA. Also running before opening

--- srv/lib/alpaca-service.js
+++ srv/lib/alpaca-service.js
@@ -4,23 +4,6 @@ class AlpacaService {
   constructor(alpaca) {
     // Alpaca instance
     this._alpaca = alpaca;
-
-    // News Socket logs.
-    // Consumer needs to only connect and subscribe
-    this._news_socket = alpaca.news_stream;
-
-    this._news_socket.onConnect(() => {
-      console.log("News Socket Connected");
-    });
-
-    this._news_socket.onError((err) => {
-      console.log("News Socket Error:", err);
-    });
-
-    this._news_socket.onDisconnect(() => {
-      console.log("News Socket Disconnected");
-    });
-
   }
 
   static getInstance() {
@@ -42,27 +25,6 @@ class AlpacaService {
 
   get newsSocket() {
     return this._news_socket;
-  }
-
-  async getTopAssetsWithMoreVolume(top) {
-    // Get all active assets from Alpaca API
-    const assets = await this.api.getAssets({ status: 'active', asset_class: "us_equity" })
-
-    const tradeableSymbols = assets
-      .filter(asset => asset.tradable)
-      .map(asset => asset.symbol);
-
-    const snapshots = await this.api.getSnapshots(tradeableSymbols);
-
-    // Filter assets based on criteria
-    return snapshots
-      .filter(s => !!s.DailyBar?.Volume)
-      .sort((a, b) => {
-        return Number(b.DailyBar?.Volume) - Number(a.DailyBar?.Volume);
-      })
-      .slice(0, top)
-      .map(snapshot => snapshot.symbol);
-
   }
 
 }
