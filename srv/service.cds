72bfe8d6215e384bfb47cb96fd2451c2c6073634
shgarcia <santiago.garcia@un.org>
2025-09-12 09:22:07 -0400

    news websocket

--- srv/service.cds
+++ srv/service.cds
@@ -1,3 +1,10 @@
+using { my.stocks as stocks } from './types/types.cds';
+
 service GPTService {
+
+  /**
+   * Returns structured data for the given stock symbol
+   */
+  function symbolData(symbol: String) returns stocks.SymbolData;
 
 }
--- srv/service.js
+++ srv/service.js
@@ -1,4 +1,4 @@
-// import cds from '@sap/cds';
+//import cds from '@sap/cds';
 
 import dotenv from 'dotenv';
 import NewsHandler from './NewsHandler.js';
@@ -12,3 +12,15 @@ const newsHandler = new NewsHandler({
 
 //Start AI Portfolio Manager
 newsHandler.start();
+
+export default function () {
+
+  //
+  // Get Symbol Data
+  //
+  this.on('symbolData', async (req) => {
+
+    return await newsHandler.getAllDataForSymbol(req.data.symbol);
+  });
+
+}
